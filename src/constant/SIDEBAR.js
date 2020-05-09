b83494e14ea9a287dd19591fcdf300589c3ba6d1
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-08 21:07:00 -0500

    json routes

---
+++ src/constant/SIDEBAR.js
@@ -0,0 +1,14 @@
+const SIDEBAR = [
+    {
+        "key"  : 1,
+        "path" : "/profesores",
+        "name" : "Profesores"
+    },
+    {
+        "key"  : 2,
+        "path" : "/asistencias",
+        "name" : "Asistencias"
+    }
+]
+
+export default SIDEBAR;
