3f6f34768c82736eefcf51ae46f0231ab28952c1
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-08 21:09:18 -0500

    routes config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import LayoutAssistance from './layout/LayoutAssistance';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
-
+        <BrowserRouter>
+            <LayoutAssistance/>
+        </BrowserRouter>
     </div>
   );
 }
