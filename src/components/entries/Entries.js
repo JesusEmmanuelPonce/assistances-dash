d2c18a31e57c4dbbba5965edd64fb045db20ffdf
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-10 13:44:03 -0500

    Entries components finished

--- src/components/entries/Entries.js
+++ src/components/entries/Entries.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import Header from '../header/Header';
 
 const Entries = () => {
+
+    const[entries, setEntries] = useState([]);
+
+    useEffect(()=>{
+        const getEntries = async() =>{
+            const resultado = await axios.get('http://localhost:3001/entries')
+            setEntries(resultado.data);
+        }
+        getEntries()
+    },[])
+
     return(
-        <h1>Entradas</h1>
+        <>
+            <Header title="Asistencias"/>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Dia</th>
+                    <th>Entrada</th>
+                    <th>Nombre</th>
+                    <th>Apellidos</th>
+                </tr>
+                </thead>
+                <tbody>
+                   {
+                       entries.map(item=>(
+                        <tr>
+                            <td>{item._id}</td>
+                       <td>{item.day}</td>
+                       <td>{item.hour}</td>
+                       <td>{item.teacher.names}</td>
+                       <td>{item.teacher.last_names}</td>
+                        </tr>
+                       ))
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
