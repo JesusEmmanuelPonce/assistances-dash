ba6e6861e69366fef93cc35f696781b9687ea2b2
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-11 15:24:20 -0500

    New teacher component added to the router

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route} from 'react-router-dom';
 import Teacher from '../components/teachers/Teacher';
 import Entries from '../components/entries/Entries';
+import NewTeacher from '../components/newTeacher/NewTeacher';
 
 const Routing = () => {
     return(
         <Switch>
             <Route exact path="/profesores" component={Teacher}/>
             <Route exact path="/asistencias" component={Entries}/>
+            <Route exact path="/nuevo-profesor" component={NewTeacher}/>
         </Switch>
     )
 }
