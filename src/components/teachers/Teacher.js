983188d9e5d97893c304f24530637241525962d2
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-16 16:22:45 -0500

    Show all user and edit module

--- src/components/teachers/Teacher.js
+++ src/components/teachers/Teacher.js
@@ -1,71 +1,85 @@
 import React, { useState, useEffect } from 'react';
 import './teacher.css';
-import { Table, Tag, Space} from 'antd';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 import Header from '../header/Header';
 import ButtonAdd from '../button/ButtonAdd';
 
 const Teacher = () => {
 
-    const TEACHER_COLUMNS = [
-        {
-          title: 'ID',
-          dataIndex: '_id',
-          key: '_id'
-        },
-        {
-          title: 'Nombre',
-          dataIndex: 'names',
-          key: 'names'
-        },
-        {
-          title: 'Apellidos',
-          dataIndex: 'last_names',
-          key: 'last_names'
-        },
-        {
-          title: 'Status',
-          dataIndex: 'status',
-          key: 'status',
-          render : status => {
-            let color = status === 'Matutino' ? 'green' : 'blue';
-            return (
-              <Tag color={color}>
-                  {status}
-              </Tag>
-            );
-          }
-        },
-        {
-          title: 'Acciones',
-          dataIndex: 'status',
-          key: 'action',
-          render: status => (
-            <Space size="middle">
-               <button onClick={()=>{
-                 alert('Work')
-               }}>{ status ? 'Desactivar' : 'Activar'}</button>
-            </Space>
-          ),
-        }
-      ];
-
     const[teacher, setTeacher] = useState([]);
 
     useEffect(()=>{
         getTeacher();
-    },[])
+    },[teacher])
 
     const getTeacher = async() => {
         const resultado = await axios.get('http://localhost:3001/teachers');
         setTeacher(resultado.data);
     }
 
+    const deleteTeacher = id =>{
+        Swal.fire({
+          title: '¿Estas seguro?',
+          text: "Profesor eliminado no se recuperara",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Si',
+          cancelButtonText: 'No'
+        }).then((result) => {
+          if (result.value) {
+            axios.delete(`http://localhost:3001/teachers/${id}`)
+              .then(res=>{
+                Swal.fire(
+                  'Eliminado',
+                  res.data.msg,
+                  'success'
+                )
+              })
+          }
+        })
+    }
+
     return(
         <>
           <Header title="Profesores"/>
             <div className="table-pb">
-                <Table columns={TEACHER_COLUMNS} dataSource={teacher} />
+              <table>
+                <thead>
+                  <tr>
+                      <th>Id</th>
+                      <th>Nombres</th>
+                      <th>Apellidos</th>
+                      <th>Status</th>
+                      <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { teacher.map(item=>(
+                      <tr key={item._id}>
+                        <td>{item._id}</td>
+                        <td>{item.names}</td>
+                        <td>{item.last_names}</td>
+                        <td>{item.status}</td>
+                        <td>
+                       
+                          <button className="btn waves-effect yellow btn-action">
+                            <i className="material-icons left">edit</i>Editar
+                          </button>
+                          <button 
+                          className="btn waves-effect red"
+                          onClick={()=>deleteTeacher(item._id)}
+                                >
+                            <i className="material-icons left">delete</i>Eliminar
+                          </button>
+                        </td>
+                      </tr>
+                    ))     
+                  }
+                </tbody>
+              </table>
             </div>
             <ButtonAdd/>
         </>
