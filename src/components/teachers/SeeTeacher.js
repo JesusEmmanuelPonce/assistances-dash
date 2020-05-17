c95fa2b078bd60d69ecc10b1c874159059c26323
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-17 10:20:06 -0500

    See teacher assistances

---
+++ src/components/teachers/SeeTeacher.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+import './teacher.css';
+import axios from 'axios';
+import Header from '../header/Header';
+
+const SeeTeacher = (props) => {
+
+    const { id } = props.match.params;
+
+    const [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+        getEntries();
+    }, [])
+
+    const getEntries = async() => {
+        const resultado = await axios.get('http://localhost:3001/entries');
+        setEntries(resultado.data);
+    }
+
+   
+console.log(entries)
+    return ( 
+        <>
+            <Header title='Asistencias'/>
+            
+            {
+                entries.map(item=>(
+                    <div>
+                        {
+                            item.teacher._id === id 
+                            ? <div className="see-assistances" key={item._id}>
+                                <span className="date">Dia:  </span>{item.day}
+                                <span className="date"> Mes:  </span>{item.month}
+                                <span className="date"> Hora: </span>{item.hour} 
+                              </div> : null
+                        }
+                    </div>
+                ))
+            }
+        </>
+     );
+}
+ 
+export default SeeTeacher;
